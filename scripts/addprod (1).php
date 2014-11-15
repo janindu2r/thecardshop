8746fee0c8d1875a76cb6247486d6d58b4513934
HPBmla <bimlamadhavee@gmail.com>
2014-11-15 22:23:18 +0530

    pro.php

---
+++ scripts/addprod (1).php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form name="addproduct" method="post" action="">
+Product ID <input type="text" name="pro_ID" /><br /><br />
+
+Product name<input type="text" name="pro_name" /><br /><br />
+
+Product description<textarea name="description" rows="3" cols="35"></textarea><br /><br />
+Product price<input type="text" name="pro_price" /><br /><br />
+Selling unit<input type="text" name="sel_unit" /><br /><br />
+Stock<input type="text" name="stock" /><br /><br />
+Product image <input type="file" name="filetoupload" id="filetoupload" /><br /><br />
+<input type="submit" name="submit" value="add product" />
+<input type="reset" name="reset" value="back" /><br /><br />
+
+
+
+
+</form>
+
+</body>
+</html>
