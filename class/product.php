1c81c2ed4bf40eb6907516a931085678d0b1d81f
HPBmla <bimlamadhavee@gmail.com>
2014-11-17 12:46:19 +0530

    recent

--- class/product.php
+++ class/product.php
@@ -8,6 +8,7 @@ class product
 	
 public $prodId, $proName,$proImg, $proPrice,$sell_Unit,$description,$stock;
 public $db;
+
 function __construct()
 {
 	try
@@ -16,7 +17,7 @@ function __construct()
 	}
 	catch(Exception $e)
 	{
-		echo "".$e->getMessage();
+		echo $e->getMessage();
 	}
 	
 }
@@ -25,11 +26,11 @@ function __construct()
 	{
 		$instance = new self();
 		$db = new DbCon();
-		$num = $db->getFirstRow("select * from products where product_ID =".$proid);
-	$instance->proName = $num['product_fname'];
+		$num = $db->getFirstRow("select * from products where product_id =".$proid);
+	$instance->proName = $num['product_title'];
 	$instance->proImg = $num['default_img_loc'];
 	$instance->proPrice = $num['price'];
-	$instance->sellUnit = $num['selling_unit'];
+	$instance->sell_Unit = $num['selling_unit'];
 	$instance->description = $num['product_desc'];
 	return $instance;	
 	}
@@ -37,16 +38,70 @@ function __construct()
 	{
 		$instance = new self();
 		$db = new DbCon();
-		$table = "";
-		$fields ="";
-		$assArr = $db->runInsertRecord($table,$fields);
-		$instance->proId = $assArr['product_id'];
-		$instance->proName = $assArr['product_name'];
-		$instance->proPrice = $assArr['product_price'];
+		
+       
+		
+		$new = $db->runInsertRecord('products',$assArr);
+		try
+		{
+			if($new = 0)
+			{
+				
+		$instance->prodId = $assArr['product_id'];
+		$instance->proName = $assArr['product_title'];
+		$instance->proPrice = $assArr['price'];
 		$instance->description = $assArr['description'];
-		$instance->sellUnit = $assArr['selling_unit'];
-		$instance->prodImg = $assArr['default_img_loc'];
+		$instance->sell_Unit = $assArr['selling_unit'];
+		$instance->proImg = $assArr['default_img_loc'];
 		return $instance;
+		}
+		}
+		catch(Exception $e)
+		{
+			echo $e->getMessage();
+			
+		}
+	}
+	
+	
+	
+	//a costructor for addProduct method
+	
+	function insertproduct($prodId,$proName,$proPrice,$sell_Unit,$description,$proImg,$stock)
+	{
+	$asscArry['product_id'] = $prodId;	
+	$asscArry['product_title'] = $proName;
+	$asscArry['price'] = $proPrice;
+	$asscArry['selling_unit'] = $sell_Unit;
+	$asscArry['description'] = $description;
+	$asscArry['stock'] = $stock;
+	return $asscArry;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	//viewing a selected product
+	  function viewProduct($prodName)
+	{
+		
+		$db = new DbCon();
+		
+		$view = $db->getFirstRow("select * from products where product_title =".$prodName);
+	
+		$this->prodId = $view["product_id"];
+		$this->proName = $view["product_title"];
+		$this->proPrice = $view["price"];
+		$this->proImg = $view["defualt_img_loc"];
+		$this->sell_Unit = $view["selling_unit"];
+		$this->description = $view["product_desc"];
+		return $this;
+		
 	}
 	
 }
--- testclass/producttest.php
+++ testclass/producttest.php
@@ -8,16 +8,16 @@ $sampleProd = product::selectProduct(1);
 echo $sampleProd->prodId ."<br>";
 echo $sampleProd->proName."<br>";
 echo $sampleProd->proPrice."<br>";
-echo $sampleProd->sellUnit."<br>";
+echo $sampleProd->sell_Unit."<br>";
 echo $sampleProd->description."<br>";
 echo $sampleProd->proImg."<br>";
 
 //initializing associative array
- 
- $arr= array('product_id'=>'2','product_title'=>'biscuits','price'=>'300','selling_unit'=>'20','product_desc'=>'its nice');
+ $arr = "o";
+ $arr= array("product_id"=>"5","product_title"=>"biscuits","price"=>"300","selling_unit"=>"20","product_desc"=>"'its nice'");
 $addedProd = product::addProduct($arr);
-echo $addedProd->proName."<br>";
-echo $addedProd->proId."<br>";
+/*echo $addedProd->proName."<br>";
+echo $addedProd->prodId."<br>";*/
 
 //inserting values to product table
 
@@ -26,5 +26,14 @@ echo $addedProd->proId."<br>";
 echo $arr['product_title']."<br>";
 echo $arr['product_id']."<br>";
 
+
+//viewing
+$viewPro = product::viewProduct(2);
+echo $viewPro->prodId."<br>";
+echo $viewPro->proName."<br>";
+echo $viewPro->description."<br>";
+echo $viewPro->proImg."<br>";
+echo $viewPro->sellUnit."<br>";
+echo $viewPro->proPrice."<br>";
 
 ?>
