0da0875c756e2c014cca42a74f774015679b29b9
discordia28 <kehelwala_j@outlook.com>
2014-11-22 18:26:03 +0530

    sample page

--- samplepage.php
+++ samplepage.php
@@ -11,14 +11,14 @@ $title = 'Sample Page' ;  // page title
 	<head>
         <?php include('header.php'); ?>
 <!---------------------------------------- Add Page Edits Below ------------------------------------------------->    
-  <div id="tempdiv">
+<div id="tempdiv">
   
   Your HTML Code per each page
   
   
   
    
-  </div><!-- temp div end -->
+</div><!-- temp div end -->
 <!---------------------------------------- End of page edits ---------------------------------------------------->
 <?php include('footer.php'); //including the footer?>
 <!-- End of page -->
