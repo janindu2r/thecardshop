495ff9808ed12b9217da926afcf994c24e004622
HPBmla <bimlamadhavee@gmail.com>
2014-11-30 11:09:01 +0530

    shopScript

---
+++ scripts/shop.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form name="addproduct" method="post" action="addShop.php">
+Shop ID <input type="text" name="sId" /><br />
+Shop Name <input type="text" name="sName" /><br />
+Account nummber<input type="text" name="sCredit" /><br />
+Background Color<input type="text" name="sColor" /><br />
+Positive points<input type="text" name="sPPoints" /><br />
+Negative points<input type="text" name="sNPoints" /><br />
+Shop base location<input type="text" name="sLoc" /><br />
+Money Guarant <input type="checkbox" name="guarant" value="yes" /> Yes
+<input type="checkbox" name="guarant" value="no" />No <br />
+Activated Date <input type="date" name="sDate" /><br />
+
+<input type="submit" value="Add Shop" />
+
+ 
+
+</form>
+</body>
+</html>
