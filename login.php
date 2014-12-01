567208a702f52abf3c5e8942ebdd28c52a6445c7
a-aslam <ahmadaslam99@yahoo.com>
2014-12-01 19:23:03 +0530

    Revert "minor change in registration and login"
    This reverts commit cf34ad76841e2e974a6348f59967b055beb4d816.

--- login.php
+++ login.php
@@ -20,11 +20,11 @@ $title = 'Login' ;  // page title
     	<div class="row">
     	    <div class="col-md-12">
         	    <div class="form-wrap">
-                <h1>Log in</h1>
+                <h1>Log in with your email account</h1>
                     <form role="form" action="/index.php" method="post" id="login-form" autocomplete="off">
                         <div class="form-group">
                             <label for="email" class="sr-only">Email</label>
-                            <input type="email" name="eid" id="email" class="form-control" placeholder="Username/Email">
+                            <input type="email" name="eid" id="email" class="form-control" placeholder="somebody@example.com">
                         </div>
                         <div class="form-group">
                             <label for="key" class="sr-only">Password</label>
