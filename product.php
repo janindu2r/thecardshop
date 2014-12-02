73144b8f219baef7d40ddde71541aba78f352e83
Janindu Ranawake <janindu93@gmail.com>
2014-12-02 20:17:14 +0530

    product page, edit to navbar styles

---
+++ product.php
@@ -0,0 +1,22 @@
+<!-- add session initiation and other similar necessary php stuff below -->
+<?php
+include('overhead.php');
+$title = 'Comercio' ; //define page title
+?>
+<!-- **************************** Header Start, Do not touch **************************** -->
+<!DOCTYPE html>
+<html>
+	<head>
+        <?php include('header.php'); ?>
+<!-- *****************************    Add Page Edits Below   **************************** -->    
+
+<div class="wrapper">
+	
+
+</div><!--  /.END OF WRAPPER -->
+	
+
+
+<!-- *****************************      End of page edits 	 **************************** -->
+<?php include('footer.php'); //including the footer?>
+<!-- End of page
