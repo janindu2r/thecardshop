bfb7d39fba1b8aae7fa1733d91006f0267e8209b
Janindu Ranawake <janindu93@gmail.com>
2014-12-03 22:58:38 +0530

    added userdashboard.php page and header link updates

---
+++ dashboarduser.php
@@ -0,0 +1,31 @@
+<!-- add session initiation and other similar necessary php stuff below -->
+<?php
+include('overhead.php');
+$title = 'Comercio' ; //define page title
+?>
+<!-- **************************** Header Start, Do not touch **************************** -->
+<!DOCTYPE html>
+<html>
+	<head>
+        <?php include('header.php'); ?>
+<!-- *****************************    Add Page Edits Below   **************************** -->    
+<div class="container">
+	<div class="col-md-12">
+		<ul class="nav nav-tabs">
+ 
+		  <li role="presentation" class="dropdown">
+		    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
+		      Dropdown <span class="caret"></span>
+		    </a>
+		    <ul class="dropdown-menu" role="menu">
+		      ...
+		    </ul>
+		  </li>
+  ...
+		</ul>
+	</div>
+</div>
+
+<!-- *****************************      End of page edits 	 **************************** -->
+<?php include('footer.php'); //including the footer?>
+<!-- End of page -->
