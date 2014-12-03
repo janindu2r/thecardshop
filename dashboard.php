5440dd0ee443f2ad41bb8dabab9ebf9f0575c72e
Janindu Ranawake <janindu93@gmail.com>
2014-12-03 23:05:10 +0530

    no message

--- dashboard.php
+++ dashboard.php
@@ -4,20 +4,29 @@ $title = 'Profile | Comercio'  ;  // page title
 
 
 ?>
-<!---------------------------------------- Header Start, Do not touch ------------------------------------------->
+<!-- -------------------------------------- Header Start, Do not touch ----------------------------------------- -->
 <!DOCTYPE html>
 <html>
 	<head>
         <?php include('header.php'); ?>
-<!---------------------------------------- Add Page Edits Below ------------------------------------------------->    
+<!-- -------------------------------------- Add Page Edits Below ----------------------------------------------- -->    
 
-Profile Page/ Account Settings page
-
-Recent Comments, Buyer Ratings, 
-
-Only visible to owner, Activate Shop button, Delete account button, Edit User Details, Notifications, Emails 
-view recent orders etc etc
-
-<!---------------------------------------- End of page edits ---------------------------------------------------->
+<div class="container">
+	<div class="col-md-12">
+		<ul class="nav nav-tabs">
+ 
+		  <li role="presentation" class="dropdown">
+		    <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">
+		      Dropdown <span class="caret"></span>
+		    </a>
+		    <ul class="dropdown-menu" role="menu">
+		      ...
+		    </ul>
+		  </li>
+  ...
+		</ul>
+	</div>
+</div>
+<!-- -------------------------------------- End of page edits -------------------------------------------------- -->
 <?php include('footer.php'); //including the footer?>
 <!-- End of page -->
