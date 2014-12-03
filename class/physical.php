6691c50ef583e135c6202efbfdce403733c56808
HPBmla <bimlamadhavee@gmail.com>
2014-12-03 18:47:38 +0530

    physicalFix

--- class/physical.php
+++ class/physical.php
@@ -147,6 +147,80 @@ echo " weight is:".$wght. "<br>";
 	
 	
 }*/
+
+
+
+
+public function initialize(array $assPhy)
+	{   
+	$this->prodId = $assPhy['prod_id'];
+	$this->height = $assPhy['height'];
+	$this->length = $assPhy['length'];
+	$this->weight = $assPhy['weight'];
+	$this->width = $assPhy['width'];
+	$this->shipCst = $assPhy['shipping_cost'];
+	$this->multiByq = $assPhy['multiply_byq'];
+	return $this;
+		
+		
+	}
+
+	//inserting data
+	
+	public function insertValue($pPid,$pWidth,$pLength,$pHeight,$pWeight,$shipCost,$multi)
+	{
+		$assArry['prod_id'] = $this->db->escapeString($pPid);
+	$assArry['width'] = $this->db->escapeString($pWidth);	
+	$assArry['length'] = $this->db->escapeString($pLength);
+	$assArry['height'] = $this->db->escapeString($pHeight);
+	$assArry['weight'] = $this->db->escapeString($pWeight);
+	$assArry['shipping_cost'] = $this->db->escapeString($shipCost);
+	$assArry['multiply_byq'] = $this->db->escapeString($multi);
+	return $this->addProduct($assArry);	
+		
+	}
+
+
+ public function addProduct(array $assc)
+ {
+
+		$add = $this->db->runInsertRecord("physical",$assc);
+	try
+	{
+		if($add == 1)
+		{
+		$this->initialize($assc);		
+		return $this;
+		}
+		else
+		return 0;
+		
+	}
+	catch(Exception $e)
+	{
+	  echo $e->getMessage();	
+	}
+		
+		
+	}
+
+
+public function updateProduct(array $value,$whereAs)
+	{
+		$pResult = $this->db->runUpdateRecord("physical",$value,$whereAs);
+		return $pResult;
+		
+		
+		
+	}
+	
+	public function deletePhyProducts($pID)
+{
+	
+		$delete = $this->db->runNonQuery("delete from physical where prod_id = ".$pID);
+	return $delete;
+}
+
 	
 	
 	
