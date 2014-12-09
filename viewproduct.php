59b489e3f7c96bde0f0a39d505c012c5b5fe7b21
Janindu Ranawake <janindu93@gmail.com>
2014-12-09 23:40:50 +0530

    Comment box style added

--- viewproduct.php
+++ viewproduct.php
@@ -149,8 +149,56 @@ $title = $viewProd->proName. ' | '. $viewfrom ;  // page title
 						
 					</div><!--  /.END OF tab-pane fade id=service-two -->
 					<div class="tab-pane fade" id="service-three">
-										Add Comments Here		
-					</div><!--  /.END OF tab-pane fade id=service-three -->
+										<div class="detailBox">
+                                            <div class="titleBox">
+                                              <label>Comment Box</label>
+                                              
+                                            </div>
+                                            <div class="commentBox">
+                                                
+                                                <p class="taskDescription">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                            </div>
+                                            <div class="actionBox">
+                                                <ul class="commentList">
+                                                    <li>
+                                                        <div class="commenterImage">
+                                                          <img src="http://lorempixel.com/50/50/people/6" />
+                                                        </div>
+                                                        <div class="commentText">
+                                                            <p class="">Hello this is a test comment.</p> <span class="date sub-text">on March 5th, 2014</span>
+
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="commenterImage">
+                                                          <img src="http://lorempixel.com/50/50/people/7" />
+                                                        </div>
+                                                        <div class="commentText">
+                                                            <p class="">Hello this is a test comment and this comment is particularly very long and it goes on and on and on.</p> <span class="date sub-text">on March 5th, 2014</span>
+
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="commenterImage">
+                                                          <img src="http://lorempixel.com/50/50/people/9" />
+                                                        </div>
+                                                        <div class="commentText">
+                                                            <p class="">Hello this is a test comment.</p> <span class="date sub-text">on March 5th, 2014</span>
+
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                                <form class="form-inline" role="form">
+                                                    <div class="form-group form-lg">
+                                                        <input class="form-control" type="text" placeholder="Your comments" />
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <button class="btn btn-default">Add</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>	
+					</div><!--  /.END **OF tab-pane fade id=service-three -->
 					
 					</div><!-- /.myTabContent -->
 					<hr>
