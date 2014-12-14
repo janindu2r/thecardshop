4035ca2e06d3e764e6352e9877708a69ee2a25a7
Janindu Ranawake <janindu93@gmail.com>
2014-12-14 09:00:43 +0530

    Checkout

---
+++ checkout.php
@@ -0,0 +1,19 @@
+<!-- add session initiation and other similar necessary php stuff below -->
+<?php
+include('overhead.php');
+
+$title = '' ;  // page title
+
+
+?>
+<!-- **************************** Header Start, Do not touch **************************** -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="/css/STEPS.styles.css">
+        <?php include('header.php'); ?>
+<!-- *****************************    Add Page Edits Below   **************************** -->    
+
+<!-- *****************************      End of page edits 	 **************************** -->
+<?php include('footer.php'); //including the footer?>
+<!-- End of page
