0726ed9f622cc3f4a8a238523651613924744d27
discordia28 <kehelwala_j@outlook.com>
2014-12-21 23:09:03 +0530

    admin

--- admin.php
+++ admin.php
@@ -190,8 +190,10 @@ $categ = substr($categ,3);
 		                    <div class="panel-body">
 			                    <div class="form-group">
                                 
-                                <form name="addproduct" method="POST" action= "/scripts/addtoproduct.php " enctype="multipart/form-data" onclick="return validateShopForm();">
-			                        <label for="#">Product Name</label>
+                            <form name="addproduct" method="POST" action= "/scripts/addtoproduct.php " enctype="multipart/form-data"  onsubmit="return validateShopForm();">
+
+                                <div class="form-group">
+                                    <label for="#">Product Name</label>
 			                        <input type="text" name = "pro_name" class="form-control" id="" placeholder="Shop john26769">
 			                    </div>
 			                    <div class="form-group">
@@ -217,7 +219,6 @@ $categ = substr($categ,3);
 						    </select>
 			                    </div>
 			                    <div class="form-group">
-                                    <div class="form-group">
 			                       <label class=" control-label" for="radios">Variations </label>
 									  <div class=""> 
 									    <label class="radio-inline" for="radios-0">
@@ -228,10 +229,10 @@ $categ = substr($categ,3);
 									      <input type="radio" name="var" id="radios-1" value="0">
 									      No
 									    </label>
-									  </div>
+                                    </div>
 			                    </div>
 			                    <div class="form-group">
-			                       <label class=" control-label" for="radios">Virtual </label>
+			                       <label class="control-label" for="radios">Virtual </label>
 									  <div class=""> 
 									    <label class="radio-inline" for="radios-0">
 									      <input type="radio" name="vir" id="radios-0" value="1" checked = "checked" >
@@ -265,14 +266,14 @@ $categ = substr($categ,3);
 									    <input id="fileUpload" name="prodimg" class="input-file" type="file">
                                       </div>
 			                    </div>
-
-
 			                    <div class="form-group">
                                     <input type="submit"  class=" btn btn-primary btn-success" id="exampleSubmit" value="Add">
                                     <input type="reset" class=" btn btn-primary btn-danger" id="exampleSubmit" value="Discard">
 			                        <!--  <a href="#" class="btn btn-primary btn-success"><span class="glyphicon glyphicon-floppy-disk"></span> Add</a>
 			                        <a href="#" class="btn btn-primary btn-danger"><span class="glyphicon glyphicon-remove"></span>Discard</a> -->
 			                    </div>
+
+                                </form>
 					    	</div>
 					    </div>
 
@@ -338,76 +339,16 @@ $categ = substr($categ,3);
 
                                 <?php }
                             } else {
-                                echo "<td colspan='8' align='center'>No New Orders</td>";
+                                echo "<tr></trd><td colspan='8' align='center'>No New Orders</td></tr>";
                             }?>
-
-
-                            <!--   <td><p><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
-                               <td><p><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-                               -->
 
                             </tbody>
 
                         </table>
-
                         <div class="clearfix"></div>
-
-
                     </div>
                 </div>
             </div>
-
-
-            <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                            <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
-                        </div>
-                        <div class="modal-body">
-                            <div class="form-group">
-                                <input class="form-control " type="text" placeholder="First Name">
-                            </div>
-                            <div class="form-group">
-
-                                <input class="form-control " type="text" placeholder="Last Name">
-                            </div>
-                            <div class="form-group">
-                                <textarea rows="2" class="form-control" placeholder="Address"></textarea>
-                            </div>
-                        </div>
-                        <div class="modal-footer ">
-                            <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
-                        </div>
-                    </div>
-                    <!-- /.modal-content -->
-                </div>
-                <!-- /.modal-dialog -->
-            </div>
-            <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                            <h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
-                        </div>
-                        <div class="modal-body">
-
-                            <div class="alert alert-warning"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this Record?</div>
-
-                        </div>
-                        <div class="modal-footer ">
-                            <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
-                            <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
-                        </div>
-                    </div>
-                    <!-- /.modal-content -->
-                </div>
-                <!-- /.modal-dialog -->
-            </div>
-
-
 
         </div>
         <div class="col-md-10 admin-content" id="charts" style="min-height:450px;">
