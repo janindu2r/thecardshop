a7b0a5fc868382dfc98be27840b1c356014361f0
discordia28 <kehelwala_j@outlook.com>
2014-12-21 23:03:32 +0530

    stf

---
+++ error.php
@@ -0,0 +1,27 @@
+<!-- add session initiation and other similar necessary php stuff below -->
+<?php
+include('/overhead.php');
+
+
+$title = 'Error | Comercio ' ;  // page title
+
+?>
+<!---------------------------------------- Header Start, Do not touch ------------------------------------------->
+<!DOCTYPE html>
+<html>
+	<head>
+        <?php include('header.php'); ?>
+<!---------------------------------------- Add Page Edits Below ------------------------------------------------->    
+
+<div class="wrapper container">
+	<div class="content-wrapper">	
+		<div class="row col-md-12 col-lg-12">
+            You seem to have encountered an error. Please go back to home page and retrace your steps.
+        </div>
+	</div><!-- /.content-wrapper -->
+</div><!-- /.wrapper container -->
+
+
+<!---------------------------------------- End of page edits ---------------------------------------------------->
+<?php include('footer.php'); //including the footer?>
+<!-- End of page -->
