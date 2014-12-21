88502510e9cf2ac9577ba73bc154f8919b79885e
a-aslam <ahmadaslam99@yahoo.com>
2014-12-21 10:40:15 +0530

    minor changes to edit profile and product

--- scripts/editproductdetails.php
+++ scripts/editproductdetails.php
@@ -12,14 +12,15 @@ include ($path.'/internal.php');
 
 $db = new DbCon();
 $obj = new Product();
-$arr['product_title'] = $db->escapeString($_POST["pro_name"]);
-$arr['price']= $db->escapeString($_POST["pro_price"]);
-$arr['product_tag'] = $db->escapeString($_POST["pro_tag"]);
-$arr['category_id'] = $db->escapeString($_POST["category"]);
-$arr['product_desc'] = $db->escapeString($_POST["description"]);
-$arr['selling_unit'] = $db->escapeString($_POST["sel_unit"]);
-$arr['initial_stck'] = $db->escapeString($_POST["stock"]);
-$arr['current_stck'] = $db->escapeString($_POST["stock"]);
+
+$arr['product_title'] = $_POST["pro_name"];
+$arr['price']= $_POST["pro_price"];
+$arr['product_tag'] = $_POST["pro_tag"];
+$arr['category_id'] = $_POST["category"];
+$arr['product_desc'] = $_POST["description"];
+$arr['selling_unit'] = $_POST["sel_unit"];
+$arr['initial_stck'] = $_POST["stock"];
+$arr['current_stck'] = $_POST["stock"];
 
 if (!empty($_POST['add']))
 {
@@ -27,34 +28,42 @@ if (!empty($_POST['add']))
     $arry;
     if($obj->proName != $arr['product_title'])
     {
+        $arr['product_title'] = $db->escapeString($_POST["pro_name"]);
         $arry['product_title'] = $arr['product_title'];
     }
     if($obj->proPrice != $arr['price'])
     {
+        $arr['price']= $db->escapeString($_POST["pro_price"]);
         $arry['price'] = $arr['price'];
     }
     if($obj->proTag != $arr['product_tag'])
     {
+        $arr['product_tag'] = $db->escapeString($_POST["pro_tag"]);
         $arry['product_tag'] = $arr['product_tag'];
     }
     if($obj->catId != $arr['category_id'])
     {
+        $arr['category_id'] = $db->escapeString($_POST["category"]);
         $arry['category_id'] = $arr['category_id'];
     }
     if($obj->description != $arr['product_desc'])
     {
+        $arr['product_desc'] = $db->escapeString($_POST["description"]);
         $arry['product_desc'] = $arr['product_desc'];
     }
     if($obj->sellUnit != $arr['selling_unit'])
     {
+        $arr['selling_unit'] = $db->escapeString($_POST["sel_unit"]);
         $arry['selling_unit'] = $arr['selling_unit'];
     }
     if($obj->inStock != $arr['initial_stck'])
     {
+        $arr['initial_stck'] = $db->escapeString($_POST["stock"]);
         $arry['initial_stck'] =$arr['initial_stck'];
     }
     if($obj->cuStock!= $arr['current_stck'])
     {
+        $arr['current_stck'] = $db->escapeString($_POST["stock"]);
         $arry['current_stck'] = $arr['current_stck'];
     }
 
--- scripts/editprofile.php
+++ scripts/editprofile.php
@@ -6,13 +6,24 @@ $user = $_SESSION['user'];
 
 $db = new DbCon();
 
-$fname = $db->escapeString($_POST["fname"]);
+$reg = $user->getRegID();
+$clause = "reg_id = '".$reg. "'";
+
+/*$fname = $db->escapeString($_POST["fname"]);
 $lname = $db->escapeString($_POST["lname"]);
 $dob = $db->escapeString($_POST["dob"]);
 $add1 = $db->escapeString($_POST["add1"]);
 $add2 = $db->escapeString($_POST["add2"]);
 $add3 = $db->escapeString($_POST["add3"]);
-$postal = $db->escapeString($_POST["postal"]);
+$postal = $db->escapeString($_POST["postal"]);*/
+
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$dob = $_POST["dob"];
+$add1 = $_POST["add1"];
+$add2 = $_POST["add2"];
+$add3 = $_POST["add3"];
+$postal = $_POST["postal"];
 
 if (!empty($_POST['submit1']))
 {
@@ -20,31 +31,46 @@ if (!empty($_POST['submit1']))
 	$arr;
 	if($user->fName != $fname)
 	{
-	$arr['fname'] = $fname;
+		$_SESSION['user']->fName = $fname;
+		$fname = $db->escapeString($_POST["fname"]);
+		$arr['fname'] = $fname;
 	}
 	if($user->lName != $lname)
 	{
+	    $_SESSION['user']->lName = $lname;
+		$lname = $db->escapeString($_POST["lname"]);
 	    $arr['lname'] = $lname;
+	}
+	if($user->getDob != $dob)
+	{
+		$_SESSION['user']->setDob($dob);
+		$dob = $db->escapeString($_POST["dob"]);
+	    $arr['dob'] = $dob;
 	}
 	if($user->postalCode != $postal)
 	{
+	    $_SESSION['user']->postalCode = $postal;
+		$postal = $db->escapeString($_POST["postal"]);
 	    $arr['postal_code'] = $postal;
 	}
 	if($user->addressL1 != $add1)
 	{
+		$_SESSION['user']->addressL1 = $add1;
+		$add1 = $db->escapeString($_POST["add1"]);
 	    $arr['deflt_billin_addl1'] = $add1;
 	}
 	if($user->addressL2 != $add2)
 	{
+		$_SESSION['user']->addressL2 = $add2;
+		$add2 = $db->escapeString($_POST["add2"]);
 	    $arr['deflt_billin_addl2'] = $add2;
 	}
 	if($user->addressL3 != $add3)
 	{
-	    $arr['deflt_billin_addl3'] = $add3;
+		$_SESSION['user']->addressL3 = $add3;
+		$add3 = $db->escapeString($_POST["add3"]);
+	    $arr['deflt_billin_addl3'] = $add3;	    
 	}
-
-	$reg = $user->getRegID();
-	$clause = "reg_id = $reg";
 
 	$res = $db->runUpdateRecord('user',$arr, $clause);
 
@@ -60,16 +86,20 @@ if (!empty($_POST['submit1']))
 
 if (!empty($_POST['submit2']))
 {
-	$uname = $db->escapeString($_POST["dispName"]);
-	$email = $db->escapeString($_POST["email"]);
+	$uname = $_POST["dispName"];
+	$email = $_POST["email"];
 
 	if($user->getDispName() != $uname)
 	{
+		$_SESSION['user']->dispName = $uname;
+		$uname = $db->escapeString($_POST["dispName"]);
 		$ar['display_name'] = $uname;
 	}
 
 	if($user->email != $email)
 	{
+		$_SESSION['user']->email = $email;
+		$email = $db->escapeString($_POST["email"]);
 		$ar['email'] = $email;
 	}
 
@@ -86,11 +116,12 @@ if (!empty($_POST['submit2']))
 }
 if (!empty($_POST['submit3']))
 {
-
+	$passsql = "Select password FROM account WHERE reg_id = '" . $reg. "'";
 	$curpass = $db->escapeString(md5($_POST["password"]));
 	$newpass = $db->escapeString(md5($_POST["newpassword"]));
+	$dbpass = $db->getScalar($passsql);
 
-	if($user->getPassword() == $curpass && $curpass != $newpass)
+	if($dbpass == $curpass && $curpass != $newpass)
 	{
 		$arra['password'] = $newpass;
 	}
