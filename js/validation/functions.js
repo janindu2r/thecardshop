7b5218bcdc7ea6ba6960133abef6fb13298d3475
HPBmla <bimlamadhavee@gmail.com>
2015-01-11 23:18:59 +0530

    jsWithReturnTypes

--- js/validation/functions.js
+++ js/validation/functions.js
@@ -2,31 +2,44 @@ function validateShopForm() {
 
     if (document.addproduct.pro_name.value.length == 0) {
         alert("please enter the product name");
+      return false;
+        
     }
     else if (document.addproduct.pro_name.value.length > 25) {
         alert("use a name with less than 25 characters");
+        return false;
     }
     else if (document.addproduct.pro_price.value == 0) {
         alert("please enter the price");
+        return false;
     }
     else if (document.addproduct.pro_tag.value.length > 50) {
         alert("please enter a product tag which has less than 50 characters");
+        return false;
     }
     else if (document.addproduct.category.selectedIndex == 0) {
         alert("please select the category");
+        return false;
     }
     else if (!(document.addproduct.var[0].checked || document.addproduct.var[1].checked)) {
         alert("please select a button for variations");
+         return false;
     }
     else if (document.addproduct.description.value.length == 0) {
         alert("please enter a product description");
+       return false;
     }
     else if (document.addproduct.sel_unit.value == 0) {
         alert("please enter selling unit");
+        return false; 
     }
     else if (document.addproduct.stock.value == 0) {
         alert("please enter stock");
+        return false;
     }
+    else
+        return true;
+  
 }
 
 function validateShopReg() {
@@ -51,6 +64,8 @@ function validateShopReg() {
         alert("please enter a payment method");
         return false;
     }
+   else
+        return true;
 }
 
 function  validateRegister() {
